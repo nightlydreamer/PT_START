dc9efaa6bf0b655617980494c328a0233538fb32
alyona <alyonka8855@mail.ru>
2024-04-25 12:48:57 +0300

    After posts

--- .config/Thunar/accels.scm
+++
@@ -1,5 +0,0 @@
-; Thunar GtkAccelMap rc-file         -*- scheme -*-
-; this file is an automated accelerator map dump
-;
-(gtk_accel_path "<Actions>/ThunarActions/uca-action-2-2" "F4")
-(gtk_accel_path "<Actions>/ThunarActions/uca-action-1-1" "F4")
